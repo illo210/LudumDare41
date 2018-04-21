8f16d7a58035d061eb9a2c941f15bd5840204c5c
illo210 <rodolphe.roussel@epitech.eu>
2018-04-21 23:27:41 +0200

    BaseTower, BaseTowerLevel, BaseProjectile, ProjectilePool first draft

---
+++ Assets/Scripts/Tower/BaseProjectile.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseProjectile : MonoBehaviour {
+
+    protected float speed = 20; // Gotta go fast
+    protected BaseEnemy _target;
+    protected bool _isUsed = true;
+
+    public void Launch(BaseEnemy enemy)
+    {
+        _target = enemy;
+    }
+
+    public void Explode()
+    {
+        // TODO Do damage
+        ProjectilePool pool = ProjectilePool.GetInstance();
+        pool.DestroyProjectile(this);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        // TODO get the position of the enemy and go in its direction
+    }
+
+    public void Free()
+    {
+        _isUsed = false;
+    }
+
+    public void MoveTo(Vector3 position)
+    {
+        transform.position = position;
+    }
+
+    // Need to be override
+    static public String GetProjectileType()
+    {
+        return "BaseProjectile";
+    }
+
+    // Need to be override
+    public bool IsSameType(BaseProjectile projectile)
+    {
+        return false;
+    }
+
+    public bool IsFree()
+    {
+        return !_isUsed;
+    }
+
+    public void Use()
+    {
+        _isUsed = true;
+    }
+
+    public static BaseProjectile Copy()
+    {
+        return new BaseProjectile();
+    }
+}
--- Assets/Scripts/Tower/BaseTower.cs
+++ Assets/Scripts/Tower/BaseTower.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class BaseTower : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public List<BaseTowerLevel> levelList;
+    public List<int> priceList;
+    protected int _level;
+
+    public int GetLevelUpPrice()
+    {
+        if (_level + 1 < priceList.Count)
+            return priceList[_level + 1];
+        return -1;
+    }
+
+    public void LevelUp()
+    {
+        if (_level + 1 < priceList.Count)
+            _level += 1;
+    }
+
+    protected void FixedUpdate()
+    {
+        levelList[_level].TowerUpdate();        
+    }
+
+    public BaseTowerLevel getCurrentLevel()
+    {
+        return levelList[_level];
+    }
+
 }
