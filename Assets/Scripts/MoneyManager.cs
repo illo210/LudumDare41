82dbd1e17dcb63ae7078559a1682ca8271cd9f1b
Tiflo <tiflozion@hotmail.fr>
2018-04-22 06:30:44 +0200

    Added simple script for money management

---
+++ Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    public int money;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void get_money(int amount)
+    {
+        money += amount;
+
+        //Display
+    }
+
+    public void give_money(int amount)
+    {
+        if (money - amount >= 0)
+        {
+            money -= amount;
+            //Display
+        }
+    }
+}
